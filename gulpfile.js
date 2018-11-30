37dc5c2410916d0ced275ea39eda6533502a45e8
Sallexx <sallexx@outlook.com>
2018-11-30 15:07:06 +0200

    first push

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,35 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 gulp.task('sass', function () {
-	return gulp.src('assets/sass/**/*.sass')
+	return gulp.src('app/sass/**/*.sass')
 		.pipe(sass())
-		.pipe(gulp.dest('build/css'))
+		.pipe(gulp.dest('dict/css'))
 		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('img', function () {
-	return gulp.src('assets/img/**/*')
-		.pipe(gulp.dest('build/img'))
+	return gulp.src('app/img/**/*')
+		.pipe(gulp.dest('dict/img'))
 		.pipe(browserSync.reload({stream: true}))
 
 });
 
 gulp.task("html", function () {
-	return gulp.src("assets/**/*.html")
-		.pipe(gulp.dest("build"))
+	return gulp.src("app/**/*.html")
+		.pipe(gulp.dest("dict"))
 		.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task("watch", [ 'sass', "html", 'img'], function () {
+gulp.task("watch", [ 'sass', "img", 'html'], function () {
 	browserSync.init({
-		server: "./build",
+		server: "./dict",
 		notify: false,
 		ui: {
 			port: 3000
 		}
     });
-    gulp.watch('assets/sass/**/*.sass', ["sass"]);
-    gulp.watch('assets/**/*.html' , ['html']);
-	gulp.watch('assets/img/**/*', ["img"]);
+    gulp.watch('app/sass/**/*.sass', ["sass"]);
+    gulp.watch('app/img/**/*', ["img"]);
+    gulp.watch('app/**/*.html' , ['html']);
+
 });
